66a3eb132698677e42fcb2d1a334805fe45c8b6d
Nikhil Pawar <ngpawar@evolenthealth.com>
2020-06-12 13:31:25 +0530

    Added new python file

---
+++ Practice.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 29 16:23:47 2020
+
+@author: NGPawar
+"""
+
+def first_fun():
+    print('hello')
+    return 
+
+first_fun()
+
+a = 'Nikhil'
+b = 'Pawar'
+c = a + '-' + b
+
+print(c)
